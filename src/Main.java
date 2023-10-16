9add9288db500e04250d5763e95a334728626ed0
felixasela <felixga1324@gmail.com>
2023-10-16 14:29:53 -0500

    add main windowOpen

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import user.loginForm;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame("Login"); // Título de la ventana
+                loginForm loginForm = new loginForm(); // Instancia de la clase loginForm
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                // Establecer el tamaño de la ventana
+                frame.setSize(800, 600);
+
+                // Hacer que la ventana sea visible
+                frame.setVisible(true);
+            }
+        });
     }
 }
