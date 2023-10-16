bcebe72ba4ca073dfdf90dfc3e15acf6fc9ec1bf
v4ld1 <dzvaldiviesod@gmail.com>
2023-10-16 15:31:47 -0500

    Ahi vamo 2

---
+++ src/admin/usuarios.java
@@ -0,0 +1,16 @@
+package admin;
+
+import javax.swing.*;
+
+public class usuarios {
+    private JTextField fieldIdUsuario;
+    private JTextField fieldNombreUsuario;
+    private JTextField fieldContraUsuario;
+    private JTextField fieldTipoUsuario;
+    private JButton editarNombreUButton;
+    private JButton editarContraButton;
+    private JButton editarTipoButton;
+    private JButton anadirUsuarioButton;
+    private JButton eliminarUsuarioButton;
+    private JTable table1;
+}
---
+++ src/databaseConexion/dbTests.java
@@ -0,0 +1,22 @@
+package databaseConexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbTests {
+    private static final String URL = "jdbc:mysql://172.17.0.1:3306/food"; //food?
+    private static final String USUARIO = "root";
+    private static final String CONTRASEÑA = "1629";
+
+    public static Connection obtenerConexion() {
+        try {
+            Connection conexion = DriverManager.getConnection(URL, USUARIO, CONTRASEÑA);
+            System.out.println("Conexión exitosa a la base de datos");
+            return conexion;
+        } catch (SQLException e) {
+            System.err.println("Error al conectar a la base de datos: " + e.getMessage());
+            return null;
+        }
+    }
+}
