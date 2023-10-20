ff4db63a4494c74ce012fc0c26cf9286a7afce7b
felixasela <felixga1324@gmail.com>
2023-10-20 10:01:08 -0500

    .

--- src/databaseConexion/dbConexion.java
+++ src/databaseConexion/dbConexion.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class dbConexion {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/foodupb";
+    private static final String URL = "jdbc:mysql://10.152.164.92:3306/foodupb";
     private static final String USUARIO = "root";
     private static final String CONTRASEÑA = "123456Vega";
 
